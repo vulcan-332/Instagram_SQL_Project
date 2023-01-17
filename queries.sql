c037fafeeb5ea1794554809c40302ff5c0dcf468
vulcan-332 <92639453+vulcan-332@users.noreply.github.com>
2023-01-17 23:01:33 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -17,3 +17,60 @@ WHERE p.id IS NULL
 /*Declaring Contest Winner: The team started a contest and the user who gets the most likes on a single photo will win the contest now they wish to declare the winner.
 Your Task: Identify the winner of the contest and provide their details to the team*/
 
+SELECT count(*), l.photo_id, u.username
+FROM photos as p
+JOIN likes as l
+ON l.photo_id = p.id
+JOIN users as u
+ON p.user_id = u.id
+GROUP BY 2
+ORDER BY 1 DESC
+LIMIT 1
+
+/*A partner brand wants to know, which hashtags to use in the post to reach the most people on the platform.
+Your Task: Identify and suggest the top 5 most commonly used hashtags on the platform*/
+
+SELECT tag_id, count(*), tag_name
+FROM photo_tags as p
+JOIN tags as t
+ON t.id = p.tag_id
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5
+
+/*The team wants to know, which day would be the best day to launch ADs.
+Your Task: What day of the week do most users register on? Provide insights on when to schedule an ad campaign*/
+
+SELECT date_format(created_at,'%W'), count(*)
+FROM users 
+GROUP BY 1
+ORDER BY 2 DESC
+
+/*Are users still as active and post on Instagram or they are making fewer posts
+Your Task: Provide how many times does average user posts on Instagram. Also, provide the total number of photos on Instagram/total number of users*/
+
+SELECT 
+    AVG(t1.counter) average_times_a_user_posts
+FROM
+    (SELECT 
+        u.username username, u.id, COUNT(p.id) counter
+    FROM
+        photos AS p
+    JOIN likes AS l ON l.photo_id = p.id
+    JOIN users AS u ON p.user_id = u.id
+    GROUP BY 1 , 2
+    ORDER BY 3 DESC) t1
+
+
+SELECT 
+    count(p.id)/count(u.id)
+FROM
+    photos AS p
+        JOIN
+    likes AS l ON l.photo_id = p.id
+        JOIN
+    users AS u ON p.user_id = u.id
+
+
+
+
